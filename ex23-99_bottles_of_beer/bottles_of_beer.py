d171402a9e270b2122382ccaa83ca9f491c93b0e
Kristopher Gates <kdlgates@kdlgates.com>
2024-09-02 13:42:37 -0400

    Fix nested curly braces in fstring in ex23

--- ex23-99_bottles_of_beer/bottles_of_beer.py
+++ ex23-99_bottles_of_beer/bottles_of_beer.py
@@ -1,9 +1,10 @@
 def print_song() -> None:
     for n in reversed(range(1, 100)):
-        print(f'{n} {'bottles' if n > 1 else 'bottle'} of beer on the wall,')
-        print(f'{n} {'bottles' if n > 1 else 'bottle'} of beer,')
-        print(f'Take one down,')
-        print(f'Pass it around,')
+        bottle_word = 'bottles' if n > 1 else 'bottle'
+        print(f'{n} {bottle_word} of beer on the wall,')
+        print(f'{n} {bottle_word} of beer,')
+        print('Take one down,')
+        print('Pass it around,')
         if n > 1:
             print(f'{n-1} bottles of beer on the wall,')
             print()
